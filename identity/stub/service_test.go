dd90e217458c5a7761ec3c24f2d63bfa77a23cb3
John Arbash Meinel <john@arbash-meinel.com>
2012-10-31 18:41:52 +0100

    Refactor tests to use common helpers for asserting failures.

--- identity/stub/service_test.go
+++ identity/stub/service_test.go
@@ -25,90 +25,81 @@ func (s *HTTPSuite) TestHelloWorld(c *C) {
 	c.Check(string(content), Equals, "Hello World\n")
 }
 
-func (s *HTTPSuite) TestLegacyIdentityServiceFailedAuth(c *C) {
-	s.mux.Handle("/", NewLegacyIdentityService(""))
-	// No headers set for Authentication
-	response, err := http.Get(s.server.URL)
-	c.Assert(err, IsNil)
+func (s *HTTPSuite) setupLegacyIdentityService(user, secret string) (token, managementURL string) {
+	managementURL = s.server.URL
+	identity := NewLegacyIdentityService(managementURL)
+	s.mux.Handle("/", identity)
+	token = "new-special-token"
+	if user != "" {
+		identity.AddUser(user, secret, token)
+	}
+	return
+}
+
+func DoAuthRequest(URL, user, key string) (*http.Response, error) {
+	client := &http.Client{}
+	request, err := http.NewRequest("GET", URL, nil)
+	if err != nil {
+		return nil, err
+	}
+	if user != "" {
+		request.Header.Set("X-Auth-User", user)
+	}
+	if key != "" {
+		request.Header.Set("X-Auth-Key", key)
+	}
+	return client.Do(request)
+}
+
+func AssertUnauthorized(c *C, response *http.Response) {
 	content, err := ioutil.ReadAll(response.Body)
+	c.Assert(err, IsNil)
 	response.Body.Close()
 	c.Check(response.Header.Get("X-Auth-Token"), Equals, "")
+	c.Check(response.Header.Get("X-Server-Management-Url"), Equals, "")
 	c.Check(string(content), Equals, "")
 	c.Check(response.StatusCode, Equals, http.StatusUnauthorized)
+}
+
+func (s *HTTPSuite) TestLegacyIdentityServiceFailedAuth(c *C) {
+	s.setupLegacyIdentityService("", "")
+	// No headers set for Authentication
+	response, err := DoAuthRequest(s.server.URL, "", "")
+	c.Assert(err, IsNil)
+	AssertUnauthorized(c, response)
 }
 
 func (s *HTTPSuite) TestIdentityServiceLegacyFailedOnlyUser(c *C) {
-	s.mux.Handle("/", NewLegacyIdentityService(""))
-	// No headers set for Authentication
-	client := &http.Client{}
-	request, err := http.NewRequest("GET", s.server.URL, nil)
+	s.setupLegacyIdentityService("", "")
+	// Missing secret key
+	response, err := DoAuthRequest(s.server.URL, "user", "")
 	c.Assert(err, IsNil)
-	request.Header.Set("X-Auth-User", "user")
-	response, err := client.Do(request)
-	c.Assert(err, IsNil)
-	content, err := ioutil.ReadAll(response.Body)
-	response.Body.Close()
-	c.Check(response.Header.Get("X-Auth-Token"), Equals, "")
-	c.Check(response.Header.Get("X-Server-Management-Url"), Equals, "")
-	c.Check(string(content), Equals, "")
-	c.Check(response.StatusCode, Equals, http.StatusUnauthorized)
+	AssertUnauthorized(c, response)
 }
 
 func (s *HTTPSuite) TestIdentityServiceLegacyNoSuchUser(c *C) {
-	s.mux.Handle("/", NewLegacyIdentityService(""))
-	// No headers set for Authentication
-	client := &http.Client{}
-	request, err := http.NewRequest("GET", s.server.URL, nil)
+	s.setupLegacyIdentityService("user", "key")
+	// No user matching the username
+	response, err := DoAuthRequest(s.server.URL, "notuser", "key")
 	c.Assert(err, IsNil)
-	request.Header.Set("X-Auth-User", "nouser")
-	request.Header.Set("X-Auth-Key", "key")
-	response, err := client.Do(request)
-	c.Assert(err, IsNil)
-	content, err := ioutil.ReadAll(response.Body)
-	response.Body.Close()
-	c.Check(response.Header.Get("X-Auth-Token"), Equals, "")
-	c.Check(response.Header.Get("X-Server-Management-Url"), Equals, "")
-	c.Check(string(content), Equals, "")
-	c.Check(response.StatusCode, Equals, http.StatusUnauthorized)
+	AssertUnauthorized(c, response)
 }
 
 func (s *HTTPSuite) TestIdentityServiceLegacyInvalidAuth(c *C) {
-	serverURL := "http://test/url"
-	identity := NewLegacyIdentityService(serverURL)
-	s.mux.Handle("/", identity)
-	identity.AddUser("user", "secret-key", "spec-token")
-	// No headers set for Authentication
-	client := &http.Client{}
-	request, err := http.NewRequest("GET", s.server.URL, nil)
+	s.setupLegacyIdentityService("user", "secret-key")
+	// Wrong key
+	response, err := DoAuthRequest(s.server.URL, "user", "bad-key")
 	c.Assert(err, IsNil)
-	request.Header.Set("X-Auth-User", "user")
-	request.Header.Set("X-Auth-Key", "bad-key")
-	response, err := client.Do(request)
-	c.Assert(err, IsNil)
-	content, err := ioutil.ReadAll(response.Body)
-	response.Body.Close()
-	c.Check(response.Header.Get("X-Auth-Token"), Equals, "")
-	c.Check(response.Header.Get("X-Server-Management-Url"), Equals, "")
-	c.Check(string(content), Equals, "")
-	c.Check(response.StatusCode, Equals, http.StatusUnauthorized)
+	AssertUnauthorized(c, response)
 }
 
 func (s *HTTPSuite) TestIdentityServiceLegacyAuth(c *C) {
-	serverURL := "http://test/url"
-	identity := NewLegacyIdentityService(serverURL)
-	s.mux.Handle("/", identity)
-	identity.AddUser("user", "secret-key", "spec-token")
-	// No headers set for Authentication
-	client := &http.Client{}
-	request, err := http.NewRequest("GET", s.server.URL, nil)
-	c.Assert(err, IsNil)
-	request.Header.Set("X-Auth-User", "user")
-	request.Header.Set("X-Auth-Key", "secret-key")
-	response, err := client.Do(request)
+	token, serverURL := s.setupLegacyIdentityService("user", "secret-key")
+	response, err := DoAuthRequest(s.server.URL, "user", "secret-key")
 	c.Assert(err, IsNil)
 	content, err := ioutil.ReadAll(response.Body)
 	response.Body.Close()
-	c.Check(response.Header.Get("X-Auth-Token"), Equals, "spec-token")
+	c.Check(response.Header.Get("X-Auth-Token"), Equals, token)
 	c.Check(response.Header.Get("X-Server-Management-Url"), Equals, serverURL)
 	c.Check(string(content), Equals, "")
 	c.Check(response.StatusCode, Equals, http.StatusNoContent)
