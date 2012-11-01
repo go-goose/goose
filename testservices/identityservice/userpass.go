33f8e77f61b6afb2aad09f8561d4c312f535e3b6
John Arbash Meinel <john@arbash-meinel.com>
2012-11-01 15:49:50 +0100

    Actually give real responses.

--- testservices/identityservice/userpass.go
+++ testservices/identityservice/userpass.go
@@ -1,6 +1,9 @@
 package identityservice
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -16,6 +19,111 @@ type UserPassRequest struct {
 	} `json:"auth"`
 }
 
+type Endpoint struct {
+	AdminURL    string `json:"adminURL"`
+	InternalURL string `json:"internalURL"`
+	PublicURL   string `json:"publicURL"`
+	Region      string `json:"region"`
+}
+
+type Service struct {
+	Name      string `json:"name"`
+	Type      string `json:"type"`
+	Endpoints []Endpoint
+}
+
+type TokenResponse struct {
+	Expires string `json:"expires"` // should this be a date object?
+	Id      string `json:"id"`      // Actual token string
+	Tenant  struct {
+		Id   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"tenant"`
+}
+
+type RoleResponse struct {
+	Id       string `json:"id"`
+	Name     string `json:"name"`
+	TenantId string `json:"tenantId"`
+}
+
+type UserResponse struct {
+	Id    string         `json:"id"`
+	Name  string         `json:"name"`
+	Roles []RoleResponse `json:"roles"`
+}
+
+type AccessResponse struct {
+	Access struct {
+		ServiceCatalog []Service     `json:"serviceCatalog"`
+		Token          TokenResponse `json:"token"`
+		User           UserResponse  `json:"user"`
+	} `json:"access"`
+}
+
+// Taken from: http://docs.openstack.org/api/quick-start/content/index.html#Getting-Credentials-a00665
+var exampleResponse = `{
+    "access": {
+        "serviceCatalog": [
+            {
+                "endpoints": [
+                    {
+                        "adminURL": "https://nova-api.trystack.org:9774/v1.1/1", 
+                        "internalURL": "https://nova-api.trystack.org:9774/v1.1/1", 
+                        "publicURL": "https://nova-api.trystack.org:9774/v1.1/1", 
+                        "region": "RegionOne"
+                    }
+                ], 
+                "name": "nova", 
+                "type": "compute"
+            }, 
+            {
+                "endpoints": [
+                    {
+                        "adminURL": "https://GLANCE_API_IS_NOT_DISCLOSED/v1.1/1", 
+                        "internalURL": "https://GLANCE_API_IS_NOT_DISCLOSED/v1.1/1", 
+                        "publicURL": "https://GLANCE_API_IS_NOT_DISCLOSED/v1.1/1", 
+                        "region": "RegionOne"
+                    }
+                ], 
+                "name": "glance", 
+                "type": "image"
+            }, 
+            {
+                "endpoints": [
+                    {
+                        "adminURL": "https://nova-api.trystack.org:5443/v2.0", 
+                        "internalURL": "https://keystone.trystack.org:5000/v2.0", 
+                        "publicURL": "https://keystone.trystack.org:5000/v2.0", 
+                        "region": "RegionOne"
+                    }
+                ], 
+                "name": "keystone", 
+                "type": "identity"
+            }
+        ], 
+        "token": {
+            "expires": "2012-02-15T19:32:21", 
+            "id": "5df9d45d-d198-4222-9b4c-7a280aa35666", 
+            "tenant": {
+                "id": "1", 
+                "name": "admin"
+            }
+        }, 
+        "user": {
+            "id": "14", 
+            "name": "annegentle", 
+            "roles": [
+                {
+                    "id": "2", 
+                    "name": "Member", 
+                    "tenantId": "1"
+                }
+            ]
+        }
+    }
+}`
+
 type UserPass struct {
 	users map[string]UserInfo
 }
@@ -30,5 +138,47 @@ func (u *UserPass) AddUser(user, secret, token string) {
 }
 
 func (u *UserPass) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusUnauthorized)
+	var req UserPassRequest
+	if r.Header.Get("Content-Type") != "application/json" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if content, err := ioutil.ReadAll(r.Body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	} else {
+		if err := json.Unmarshal(content, &req); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+	userInfo, ok := u.users[req.Auth.PasswordCredentials.Username]
+	if !ok {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	if userInfo.secret != req.Auth.PasswordCredentials.Password {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	res := AccessResponse{}
+	// We pre-populate the response with genuine entries so that it looks sane.
+	// XXX: We should really build up valid state for this instead, at the
+	//	very least, we should manage the URLs better.
+	if err := json.Unmarshal([]byte(exampleResponse), &res); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	res.Access.Token.Id = userInfo.token
+	if content, err := json.Marshal(res); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprint(err)))
+		return
+	} else {
+		w.WriteHeader(http.StatusOK)
+		w.Write(content)
+		return
+	}
+	w.WriteHeader(http.StatusInternalServerError)
+	return
 }
--- testservices/identityservice/userpass_test.go
+++ testservices/identityservice/userpass_test.go
@@ -2,6 +2,8 @@ package identityservice
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	. "launchpad.net/gocheck"
 	"launchpad.net/goose/testing/httpsuite"
 	"net/http"
@@ -26,28 +28,54 @@ func (s *UserPassSuite) setupUserPass(user, secret string) (token string) {
 	return
 }
 
+var authTemplate = `{
+    "auth": {
+        "tenantName": "tenant-something", 
+        "passwordCredentials": {
+            "username": "%s", 
+            "password": "%s"
+        }
+    }
+}`
+
 func UserPassAuthRequest(URL, user, key string) (*http.Response, error) {
 	client := &http.Client{}
-	req := UserPassRequest{}
-	req.Auth.PasswordCredentials.Username = user
-	req.Auth.PasswordCredentials.Password = key
-	req.Auth.TenantName = "tenant-something-or-other"
-	as_json, err := json.Marshal(req)
-	if err != nil {
-		return nil, err
-	}
-	body := strings.NewReader(string(as_json))
+	body := strings.NewReader(fmt.Sprintf(authTemplate, user, key))
 	request, err := http.NewRequest("POST", URL, body)
+	request.Header.Set("Content-Type", "application/json")
 	if err != nil {
 		return nil, err
 	}
 	return client.Do(request)
 }
 
-func (s *UserPassSuite) TestInvalidRequest(c *C) {
+func (s *UserPassSuite) TestNotJSON(c *C) {
+	// We do everything in UserPassAuthRequest, except set the Content-Type
 	token := s.setupUserPass("user", "secret")
 	c.Assert(token, NotNil)
-	res, err := UserPassAuthRequest(s.Server.URL, "user", "bad-secret")
+	client := &http.Client{}
+	body := strings.NewReader(fmt.Sprintf(authTemplate, "user", "secret"))
+	request, err := http.NewRequest("POST", s.Server.URL, body)
+	c.Assert(err, IsNil)
+	res, err := client.Do(request)
+	c.Assert(err, IsNil)
+	c.Check(res.StatusCode, Equals, http.StatusBadRequest)
+}
+
+func (s *UserPassSuite) TestNoSuchUser(c *C) {
+	token := s.setupUserPass("user", "secret")
+	c.Assert(token, NotNil)
+	res, err := UserPassAuthRequest(s.Server.URL, "not-user", "secret")
+	defer res.Body.Close()
+	c.Assert(err, IsNil)
+	c.Check(res.StatusCode, Equals, http.StatusUnauthorized)
+}
+
+func (s *UserPassSuite) TestBadPassword(c *C) {
+	token := s.setupUserPass("user", "secret")
+	c.Assert(token, NotNil)
+	res, err := UserPassAuthRequest(s.Server.URL, "user", "not-secret")
+	defer res.Body.Close()
 	c.Assert(err, IsNil)
 	c.Check(res.StatusCode, Equals, http.StatusUnauthorized)
 }
@@ -56,6 +84,21 @@ func (s *UserPassSuite) TestValidAuthorization(c *C) {
 	token := s.setupUserPass("user", "secret")
 	c.Assert(token, NotNil)
 	res, err := UserPassAuthRequest(s.Server.URL, "user", "secret")
+	defer res.Body.Close()
 	c.Assert(err, IsNil)
 	c.Check(res.StatusCode, Equals, http.StatusOK)
+	content, err := ioutil.ReadAll(res.Body)
+	c.Assert(err, IsNil)
+	var response AccessResponse
+	err = json.Unmarshal(content, &response)
+	c.Assert(err, IsNil)
+	c.Check(response.Access.Token.Id, Equals, token)
+	novaURL := ""
+	for _, service := range response.Access.ServiceCatalog {
+		if service.Type == "compute" {
+			novaURL = service.Endpoints[0].PublicURL
+			break
+		}
+	}
+	c.Assert(novaURL, Not(Equals), "")
 }
