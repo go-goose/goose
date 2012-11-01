dbbf24500f7a5d18edd55a5b61ced9b941b222bd
John Arbash Meinel <john@arbash-meinel.com>
2012-11-01 12:00:10 +0100

    Pull together client-side Legacy authentication.
    Test it against our dummy implementation.

--- identity/legacy.go
+++ identity/legacy.go
@@ -1,4 +1,42 @@
 package identity
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
 type Legacy struct {
+}
+
+func (l *Legacy) Auth(creds Credentials) (*AuthDetails, error) {
+	client := &http.Client{}
+	request, err := http.NewRequest("GET", creds.URL, nil)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Set("X-Auth-User", creds.User)
+	request.Header.Set("X-Auth-Key", creds.Secrets)
+	response, err := client.Do(request)
+	defer response.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode != http.StatusNoContent {
+		content, _ := ioutil.ReadAll(response.Body)
+		return nil, fmt.Errorf("Failed to Authenticate (code %d %s): %s",
+			response.StatusCode, response.Status, content)
+	}
+	details := &AuthDetails{}
+	details.Token = response.Header.Get("X-Auth-Token")
+	if details.Token == "" {
+		return nil, fmt.Errorf("Did not get valid Token from auth request")
+	}
+	nova_url := response.Header.Get("X-Server-Management-Url")
+	if nova_url == "" {
+		return nil, fmt.Errorf("Did not get valid management URL from auth request")
+	}
+	details.ServiceURLs = map[string]string{"compute": nova_url}
+
+	return details, nil
 }
--- identity/legacy_test.go
+++ identity/legacy_test.go
@@ -3,6 +3,7 @@ package identity
 import (
 	. "launchpad.net/gocheck"
 	"launchpad.net/goose/testing/httpsuite"
+	"launchpad.net/goose/testservices/identityservice"
 	"testing"
 )
 
@@ -16,5 +17,26 @@ type LegacyTestSuite struct {
 
 var _ = Suite(&LegacyTestSuite{})
 
-func (s *LegacyTestSuite) TestSomething(c *C) {
+func (s *LegacyTestSuite) TestAuthAgainstServer(c *C) {
+	service := identityservice.NewLegacy()
+	s.Mux.Handle("/", service)
+	service.AddUser("joe-user", "secrets", "active-token")
+	service.SetManagementURL("http://management/url")
+	l := Legacy{}
+	creds := Credentials{User: "joe-user", URL: s.Server.URL, Secrets: "secrets"}
+	auth, err := l.Auth(creds)
+	c.Assert(err, IsNil)
+	c.Assert(auth.Token, Equals, "active-token")
+	c.Assert(auth.ServiceURLs, DeepEquals, map[string]string{"compute": "http://management/url"})
+}
+
+func (s *LegacyTestSuite) TestBadAuth(c *C) {
+	service := identityservice.NewLegacy()
+	s.Mux.Handle("/", service)
+	service.AddUser("joe-user", "secrets", "active-token")
+	l := Legacy{}
+	creds := Credentials{User: "joe-user", URL: s.Server.URL, Secrets: "bad-secrets"}
+	auth, err := l.Auth(creds)
+	c.Assert(err, NotNil)
+	c.Assert(auth, IsNil)
 }
