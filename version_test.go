1c9cd5d33901758578dc010186f4469b68c1031c
John Arbash Meinel <john@arbash-meinel.com>
2013-02-05 13:22:51 +0000

    Write some very simple Version code for tracking the compiled version of goose.

---
+++ version_test.go
@@ -0,0 +1,15 @@
+package goose
+
+import (
+	. "launchpad.net/gocheck"
+)
+
+type VersionTestSuite struct {
+}
+
+var _ = Suite(&VersionTestSuite{})
+
+
+func (s *VersionTestSuite) TestStringMatches(c *C) {
+	c.Assert(Version, Equals, VersionNumber.String())
+}
