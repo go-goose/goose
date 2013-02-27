0c231489464f64bf2b17e73c337c9c875afbf0de
John Arbash Meinel <john@arbash-meinel.com>
2013-02-27 13:52:21 +0400

    Add tests and fix the actual bugs.
    The problem was that if Read returned less that 16 bytes, we
    would trigger the error handling, but that unconditionally
    called err.Error(). Though err is actually nil in this case.
    So we got a nil-pointer dereference, which is a bit uglier
    to try and figure out.

--- testservices/identityservice/util.go
+++ testservices/identityservice/util.go
@@ -13,14 +13,37 @@ type UserInfo struct {
 	secret   string
 }
 
+type randomizer func([]byte) (int, error)
+
+var getRandom randomizer = rand.Read
+
+// Change how we get random data, the default is to use crypto/rand
+// This mostly exists to be able to test error side effects
+// The return value is a function you can call to restore the previous
+// randomizer
+// Note: This is not thread safe, but it is really only for testing anyway
+func setRandomizer(r randomizer) (restore func()) {
+	old := getRandom
+	getRandom = r
+	return func() {
+		getRandom = old
+	}
+}
+
 // Generate a bit of random hex data for
 func randomHexToken() string {
 	raw_bytes := make([]byte, 16)
-	n, err := rand.Read(raw_bytes)
+	n, err := getRandom(raw_bytes)
 	if n != 16 || err != nil {
+		var errStr string
+		if err == nil {
+			errStr = "<unknown>"
+		} else {
+			errStr = err.Error()
+		}
 		panic(fmt.Sprintf(
-			"Could not read 16 random bytes safely: %d %s",
-			n, err.Error()))
+			"Could not read 16 random bytes safely (read %d bytes): %s",
+			n, errStr))
 	}
 	hex_bytes := make([]byte, 32)
 	n = hex.Encode(hex_bytes, raw_bytes)
--- testservices/identityservice/util_test.go
+++ testservices/identityservice/util_test.go
@@ -1,7 +1,10 @@
 package identityservice
 
 import (
+	"crypto/rand"
+	"fmt"
 	. "launchpad.net/gocheck"
+	"reflect"
 )
 
 type UtilSuite struct{}
@@ -25,4 +28,87 @@ func (s *UtilSuite) TestRandomHexTokenIsHex(c *C) {
 			c.Fail()
 		}
 	}
+}
+
+func (s *UtilSuite) TestDefaultRandomizer(c *C) {
+	raw := make([]byte, 6)
+	c.Assert(string(raw), Equals, "\x00\x00\x00\x00\x00\x00")
+	n, err := getRandom(raw)
+	c.Assert(err, IsNil)
+	c.Assert(n, Equals, 6)
+	c.Assert(string(raw), Not(Equals), "\x00\x00\x00\x00\x00\x00")
+}
+
+type funcEqualsChecker struct {
+	*CheckerInfo
+}
+
+var _ Checker = (*funcEqualsChecker)(nil)
+
+func (checker *funcEqualsChecker) Check(params []interface{}, names []string) (result bool, error string) {
+	ptr1 := reflect.ValueOf(params[0]).Pointer()
+	ptr2 := reflect.ValueOf(params[1]).Pointer()
+	return Equals.Check([]interface{}{ptr1, ptr2}, names)
+}
+
+// The Go language specification doesn't allow you to compare function pointers
+// directly, so we use a little bit of a workaround. This may break in future
+// versions of go, but it can be updated when necessary.
+// http://stackoverflow.com/questions/9643205/how-do-i-compare-two-functions-for-pointer-equality-in-the-latest-go-weekly
+var FuncEquals Checker = &funcEqualsChecker{
+	&CheckerInfo{Name: "FuncEquals", Params: []string{"obtained", "expected"}},
+}
+
+func (s *UtilSuite) TestSetRandomizer(c *C) {
+	orig := getRandom
+	// This test will be mutating global state (getRandom), ensure that we
+	// restore it sanely even if tests fail
+	defer func() { getRandom = orig }()
+	// "randomize" everything to the letter 'n'
+	nRandomizer := func(b []byte) (n int, err error) {
+		for i := range b {
+			b[i] = 'n'
+		}
+		n = len(b)
+		return
+	}
+
+	c.Assert(getRandom, FuncEquals, rand.Read)
+	cleanup := setRandomizer(nRandomizer)
+	c.Assert(getRandom, FuncEquals, nRandomizer)
+	raw := make([]byte, 6)
+	n, err := getRandom(raw)
+	c.Assert(err, IsNil)
+	c.Assert(n, Equals, 6)
+	c.Assert(string(raw), Equals, "nnnnnn")
+	cleanup()
+	c.Assert(getRandom, FuncEquals, rand.Read)
+}
+
+func (s *UtilSuite) TestNotEnoughRandomBytes(c *C) {
+	// No error, just not enough bytes
+	shortRand := func(b []byte) (n int, err error) {
+		var i = 0
+		for ; i < 2; i++ {
+			b[i] = 'x'
+		}
+		return i, nil
+	}
+	cleanup := setRandomizer(shortRand)
+	defer cleanup()
+	c.Assert(randomHexToken, PanicMatches, "Could not read 16 random bytes safely \\(read 2 bytes\\): <unknown>")
+}
+
+func (s *UtilSuite) TestRandomBytesError(c *C) {
+	// No error, just not enough bytes
+	errRand := func(b []byte) (n int, err error) {
+		var i = 0
+		for ; i < 3; i++ {
+			b[i] = 'x'
+		}
+		return i, fmt.Errorf("Not enough bytes")
+	}
+	cleanup := setRandomizer(errRand)
+	defer cleanup()
+	c.Assert(randomHexToken, PanicMatches, "Could not read 16 random bytes safely \\(read 3 bytes\\): Not enough bytes")
 }
