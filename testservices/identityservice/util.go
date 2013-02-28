0dc01dcc6979f71953203166643a7190ef2a667d
John Arbash Meinel <john@arbash-meinel.com>
2013-02-28 14:32:20 +0400

    Review feedback from Roger Peppe
    Use io.ReadFull and rand.Reader rather than rand.Read.
    I didn't see the Reader object, since variables aren't nearly
    as obviously defined in the documentation as the functions
    themselves. This ends up quite a bit nicer, and avoids the
    need for the new function.

--- testservices/identityservice/util.go
+++ testservices/identityservice/util.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"fmt"
+	"io"
 )
 
 type UserInfo struct {
@@ -13,32 +14,28 @@ type UserInfo struct {
 	secret   string
 }
 
-type randomizer func([]byte) (int, error)
-
-var getRandom randomizer = rand.Read
+var randReader = rand.Reader
 
 // Change how we get random data, the default is to use crypto/rand
 // This mostly exists to be able to test error side effects
 // The return value is a function you can call to restore the previous
 // randomizer
 // Note: This is not thread safe, but it is really only for testing anyway
-func setRandomizer(r randomizer) (restore func()) {
-	old := getRandom
-	getRandom = r
+func setReader(r io.Reader) (restore func()) {
+	old := randReader
+	randReader = r
 	return func() {
-		getRandom = old
+		randReader = old
 	}
 }
 
 // Generate a bit of random hex data for
 func randomHexToken() string {
 	raw_bytes := make([]byte, 16)
-	n, err := getRandom(raw_bytes)
+	n, err := io.ReadFull(randReader, raw_bytes)
 	if n != 16 || err != nil {
 		var errStr string
-		if err == nil {
-			errStr = "<unknown>"
-		} else {
+		if err != nil {
 			errStr = err.Error()
 		}
 		panic(fmt.Sprintf(
--- testservices/identityservice/util_test.go
+++ testservices/identityservice/util_test.go
@@ -1,10 +1,11 @@
 package identityservice
 
 import (
+	"bytes"
 	"crypto/rand"
 	"fmt"
+	"io"
 	. "launchpad.net/gocheck"
-	"reflect"
 )
 
 type UtilSuite struct{}
@@ -30,85 +31,54 @@ func (s *UtilSuite) TestRandomHexTokenIsHex(c *C) {
 	}
 }
 
-func (s *UtilSuite) TestDefaultRandomizer(c *C) {
+func (s *UtilSuite) TestDefaultReader(c *C) {
 	raw := make([]byte, 6)
 	c.Assert(string(raw), Equals, "\x00\x00\x00\x00\x00\x00")
-	n, err := getRandom(raw)
+	n, err := io.ReadFull(randReader, raw)
 	c.Assert(err, IsNil)
 	c.Assert(n, Equals, 6)
 	c.Assert(string(raw), Not(Equals), "\x00\x00\x00\x00\x00\x00")
 }
 
-type funcEqualsChecker struct {
-	*CheckerInfo
-}
-
-var _ Checker = (*funcEqualsChecker)(nil)
-
-func (checker *funcEqualsChecker) Check(params []interface{}, names []string) (result bool, error string) {
-	ptr1 := reflect.ValueOf(params[0]).Pointer()
-	ptr2 := reflect.ValueOf(params[1]).Pointer()
-	return Equals.Check([]interface{}{ptr1, ptr2}, names)
-}
-
-// The Go language specification doesn't allow you to compare function pointers
-// directly, so we use a little bit of a workaround. This may break in future
-// versions of go, but it can be updated when necessary.
-// http://stackoverflow.com/questions/9643205/how-do-i-compare-two-functions-for-pointer-equality-in-the-latest-go-weekly
-var FuncEquals Checker = &funcEqualsChecker{
-	&CheckerInfo{Name: "FuncEquals", Params: []string{"obtained", "expected"}},
-}
-
-func (s *UtilSuite) TestSetRandomizer(c *C) {
-	orig := getRandom
-	// This test will be mutating global state (getRandom), ensure that we
+func (s *UtilSuite) TestSetReader(c *C) {
+	orig := randReader
+	// This test will be mutating global state (randReader), ensure that we
 	// restore it sanely even if tests fail
-	defer func() { getRandom = orig }()
+	defer func() { randReader = orig }()
 	// "randomize" everything to the letter 'n'
-	nRandomizer := func(b []byte) (n int, err error) {
-		for i := range b {
-			b[i] = 'n'
-		}
-		n = len(b)
-		return
-	}
-
-	c.Assert(getRandom, FuncEquals, rand.Read)
-	cleanup := setRandomizer(nRandomizer)
-	c.Assert(getRandom, FuncEquals, nRandomizer)
+	nRandom := bytes.NewBufferString("nnnnnnnnnnnnnnnnnnnnnnn")
+	c.Assert(randReader, Equals, rand.Reader)
+	cleanup := setReader(nRandom)
+	c.Assert(randReader, Equals, nRandom)
 	raw := make([]byte, 6)
-	n, err := getRandom(raw)
+	n, err := io.ReadFull(randReader, raw)
 	c.Assert(err, IsNil)
 	c.Assert(n, Equals, 6)
 	c.Assert(string(raw), Equals, "nnnnnn")
 	cleanup()
-	c.Assert(getRandom, FuncEquals, rand.Read)
+	c.Assert(randReader, Equals, rand.Reader)
 }
 
 func (s *UtilSuite) TestNotEnoughRandomBytes(c *C) {
 	// No error, just not enough bytes
-	shortRand := func(b []byte) (n int, err error) {
-		var i = 0
-		for ; i < 2; i++ {
-			b[i] = 'x'
-		}
-		return i, nil
-	}
-	cleanup := setRandomizer(shortRand)
+	shortRand := bytes.NewBufferString("xx")
+	cleanup := setReader(shortRand)
 	defer cleanup()
-	c.Assert(randomHexToken, PanicMatches, "Could not read 16 random bytes safely \\(read 2 bytes\\): <unknown>")
+	c.Assert(randomHexToken, PanicMatches, "Could not read 16 random bytes safely \\(read 2 bytes\\): unexpected EOF")
+}
+
+type ErrReader struct{}
+
+func (e ErrReader) Read(b []byte) (n int, err error) {
+	b[0] = 'x'
+	b[1] = 'x'
+	b[2] = 'x'
+	return 3, fmt.Errorf("Not enough bytes")
 }
 
 func (s *UtilSuite) TestRandomBytesError(c *C) {
 	// No error, just not enough bytes
-	errRand := func(b []byte) (n int, err error) {
-		var i = 0
-		for ; i < 3; i++ {
-			b[i] = 'x'
-		}
-		return i, fmt.Errorf("Not enough bytes")
-	}
-	cleanup := setRandomizer(errRand)
+	cleanup := setReader(ErrReader{})
 	defer cleanup()
 	c.Assert(randomHexToken, PanicMatches, "Could not read 16 random bytes safely \\(read 3 bytes\\): Not enough bytes")
 }
