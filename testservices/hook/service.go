5357702db3c78398f8af684a129bf42014577412
Michael Hudson-Doyle <michael.hudson@linaro.org>
2013-11-26 09:32:40 +1300

    add some comments

--- testservices/hook/service.go
+++ testservices/hook/service.go
@@ -31,6 +31,16 @@ type ServiceControl interface {
 func (s *TestService) currentServiceMethodName() string {
 	var pc uintptr
 	var ok bool
+
+	// We have to go deeper into the stack with gccgo because in a situation like:
+	// type Inner { }
+	// func (i *Inner) meth {}
+	// type Outer { Inner }
+	// o = &Outer{}
+	// o.meth()
+	// gccgo generates a method called "meth" on *Outer, and this shows up
+	// on the stack as seen by runtime.Caller (this might be a gccgo bug).
+
 	if runtime.Compiler == "gccgo" {
 		pc, _, _, ok = runtime.Caller(3)
 	} else {
@@ -46,6 +56,10 @@ func unqualifiedMethodName(pc uintptr) string {
 	f := runtime.FuncForPC(pc)
 	fullName := f.Name()
 	if runtime.Compiler == "gccgo" {
+		// This is very fragile.  fullName will be something like:
+		// launchpad.net_goose_testservices_novaservice.removeServer.pN49_launchpad.net_goose_testservices_novaservice.Nova
+		// so if the number of dots in the full package path changes,
+		// this will need to too...
 		nameParts := strings.Split(fullName, ".")
 		return nameParts[2]
 	} else {
