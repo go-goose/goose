5a8c056a5e354876cc8accc8be5d531d77ef519c
Dave Cheney <david.cheney@canonical.com>
2014-02-06 11:42:49 +1100

    attempt to detect the caller depth automatically

--- testservices/hook/service_gccgo.go
+++ testservices/hook/service_gccgo.go
@@ -7,11 +7,42 @@ import (
 	"strings"
 )
 
-const (
-	callerDepth = 2 // gccgo 4.8
-	// callerDepth = 3 // gccgo 4.9
-	namePartsPos = 2
-)
+// callerDepth defines the number of stack frames to skip during
+// currentServiceMethodName. This value differs for various gccgo
+// versions.
+var callerDepth int
+
+type inner struct{}
+
+func (i *inner) m() {
+	for callerDepth = 1; ; callerDepth++ {
+		pc, _, _, ok := runtime.Caller(callerDepth)
+		if !ok {
+			panic("current method name cannot be found")
+		}
+		if name := runtime.FuncForPC(pc).Name(); name == "hook.setCallerDepth" {
+			return
+		}
+	}
+}
+
+type outer struct {
+	inner
+}
+
+func setCallerDepth0() {
+	var o outer
+	o.m()
+}
+
+func setCallerDepth() {
+	setCallerDepth0()
+}
+
+func init() {
+	setCallerDepth()
+	println(callerDepth)
+}
 
 // currentServiceMethodName returns the method executing on the service when ProcessControlHook was invoked.
 func (s *TestService) currentServiceMethodName() string {
@@ -23,7 +54,6 @@ func (s *TestService) currentServiceMethodName() string {
 	// o.meth()
 	// gccgo generates a method called "meth" on *Outer, and this shows up
 	// on the stack as seen by runtime.Caller (this might be a gccgo bug).
-
 	pc, _, _, ok := runtime.Caller(callerDepth)
 	if !ok {
 		panic("current method name cannot be found")
@@ -38,6 +68,7 @@ func unqualifiedMethodName(pc uintptr) string {
 	// launchpad.net_goose_testservices_novaservice.removeServer.pN49_launchpad.net_goose_testservices_novaservice.Nova
 	// so if the number of dots in the full package path changes,
 	// this will need to too...
+	const namePartsPos = 2
 	nameParts := strings.Split(fullName, ".")
 	return nameParts[namePartsPos]
 }
