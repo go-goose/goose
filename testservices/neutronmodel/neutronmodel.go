ee8b3643052127014f2a792a58a03e88e98af079
Adis Azhar <adis.azhar@canonical.com>
2025-07-08 09:34:51 +0700

    chore: make linter happy

--- testservices/neutronmodel/neutronmodel.go
+++ testservices/neutronmodel/neutronmodel.go
@@ -282,20 +282,6 @@ func (n *NeutronModel) SecurityGroupByName(groupName string) ([]neutron.Security
 	return foundGrps, nil
 }
 
-func equals(s1 []string, s2 []string) bool {
-	if len(s1) != len(s2) {
-		return false
-	}
-
-	for i := 0; i < len(s1); i++ {
-		if s1[i] != s2[i] {
-			return false
-		}
-	}
-
-	return true
-}
-
 // containsAll check whether all elements in `tags` are present in `groupTags`.
 func containsAll(groupTags, tags []string) bool {
 	tagSet := make(map[string]struct{}, len(groupTags))
